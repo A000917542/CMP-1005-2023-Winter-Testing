7a0b0be78713be71f2d7015136244d8f49a64ab3
Brent Ritchie <ritchie.brent@gmail.com>
2023-01-24 20:27:19 -0500

    Add project files.

---
+++ CMP-1005-2023-Winter-Testing-Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CMP_1005_2023_Winter_Testing_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CMP-1005-2023-Winter-Testing/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CMP_1005_2023_Winter_Testing
+{
+    public class Class1
+    {
+
+    }
+}
