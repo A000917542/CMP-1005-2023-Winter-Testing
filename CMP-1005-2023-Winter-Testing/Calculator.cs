2a8120aba519b5f637fc9ebe852ec5b034978721
Brent Ritchie <ritchie.brent@gmail.com>
2023-01-24 20:43:55 -0500

    Wrote initial implementation for #1.

---
+++ CMP-1005-2023-Winter-Testing/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CMP_1005_2023_Winter_Testing
+{
+    public static class Calculator
+    {
+        public static int Add(int left, int right)
+        {
+            return left + right;
+        }
+    }
+}
--- CMP-1005-2023-Winter-Testing/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CMP_1005_2023_Winter_Testing
-{
-    public class Class1
-    {
-
-    }
-}
